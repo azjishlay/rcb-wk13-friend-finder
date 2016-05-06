a6b7cdaf7b44ee565b1947fc34a326747470b4f0
lijasminej <li.jasmine.j@gmail.com>
2016-05-06 19:40:56 -0400

    Changed data array

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,16 @@ var friendsArray = [
         "name": "Jasmine",
         "photo": "https://avatars0.githubusercontent.com/u/16637558?v=3&s=460",
         "answers": [
-            3,
-            3,
-            3,
-            3,
-            3,
-            1,
-            5,
-            1,
-            5,
-            1
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1"
         ]
     }
 ];
