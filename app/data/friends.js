ae117d27a998a9ae087e6ef84512ca6a9bc722b6
lijasminej <li.jasmine.j@gmail.com>
2016-05-07 00:32:50 -0400

    Working match, modal popup

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friendsArray = [
     {
-        "name": "Jasmine",
-        "photo": "https://avatars0.githubusercontent.com/u/16637558?v=3&s=460",
+        "name": "Myspace Tom",
+        "photo": "http://www.eonline.com/eol_images/Entire_Site/2015518/rs_600x600-150618104510-600.tom-myspace.jw.61815_2.jpg",
         "answers": [
-            "3",
-            "3",
-            "3",
-            "3",
+            "1",
+            "2",
+            "2",
+            "1",
             "3",
             "1",
             "5",
@@ -14,7 +14,23 @@ var friendsArray = [
             "5",
             "1"
         ]
+    },
+    {
+        "name": "Kim Kardashian",
+        "photo": "http://www.eonline.com/eol_images/Entire_Site/2014428/rs_300x300-140528094720-300.Kim-Kardashian-West-Twitter.jl.052814.jpg",
+        "answers": [
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3",
+            "4",
+            "2",
+            "1",
+            "4"
+        ]
     }
 ];
 
-module.exports = friendsArray;
+module.exports = friendsArray; 
